40de5ad6db59f2a34c00389982abde2704d7b01d
Samuel Ralak <samuelralak@hotmail.com>
2018-06-29 15:03:55 +0300

    Problem: OAuth credentials purged on app start up #51
    Solution: deleted ServiceConfig block in main.js

--- server/main.js
+++ server/main.js
@@ -2,13 +2,3 @@
 
 import '/imports/startup/server'
 import '/imports/startup/both'
-
-/* required to configurate github auth for the first time */
-ServiceConfiguration.configurations.remove({
-    service: "github"
-});
-ServiceConfiguration.configurations.insert({
-    service: "github",
-    clientId: 'xxx',
-    secret: 'xxx'
-});
