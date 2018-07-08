0e4601787fbc00c9da6880879598e22fa1cf5849
Ash Smith <missenlinx@gmail.com>
2018-07-08 21:05:08 +0800

    problem: unable to select what payments to pay
    solution: add ability to select what payments should be paid #56

--- imports/ui/pages/moderator/payments/paymentsview.js
+++ imports/ui/pages/moderator/payments/paymentsview.js
@@ -15,8 +15,18 @@ const formatDuration = (duration) => {
     return `${pad(duration.hours())}:${pad(duration.minutes())}:${pad(duration.seconds())}`
 }
 
+const calculateEarnings = () => {
+    var sum = 0;
+
+    $("input[type=checkbox]:checked").each(function() {
+        sum += parseInt($(this).attr("rel"));
+    });
+
+    return sum ? sum : 0.00;
+}
 
 Template.paymentsview.onCreated(function() {
+    this.approvedEarnings = new ReactiveVar()
     this.autorun(() => {
         this.subscribe('timesheet.paymentId', FlowRouter.getParam("paymentId"))
         this.subscribe('payments')
@@ -25,18 +35,57 @@ Template.paymentsview.onCreated(function() {
 })
 
 Template.paymentsview.helpers({
-    notPaid: () => {
+    paid: () => {
         //check to see if the payment has been made.
-        let notPaid = Payments.findOne({ _id: FlowRouter.getParam("paymentId"), status: 'payment-paid' });
+        let paid = Payments.findOne({ _id: FlowRouter.getParam("paymentId"), status: 'payment-paid' });
 
-        return notPaid ? notPaid.status : null
+        return paid ? paid.status : null
     },
-    totalEarnings: () => {
-        let sum = Timesheet.find({}).fetch()
-        
-        return sum.map(v => v.totalEarnings ? v.totalEarnings : 0 ).reduce(
+    totalApprovedEarnings: () => {
+        return Template.instance().approvedEarnings.get()
+    },
+    totalPaidEarnings:  () => {
+        let sum = Timesheet.find({status:'payment-paid'}).fetch()
+
+        return sum.map(v => v.totalEarnings ? v.totalEarnings : 0).reduce(
             (acc, curr) => acc + curr, 0
         )
+    },
+    totalTimeSheetEarnings: () => {
+        let sum = Timesheet.find({}).fetch()
+
+        return sum.map(v => v.totalEarnings ? v.totalEarnings : 0).reduce(
+            (acc, curr) => acc + curr, 0
+        )
+    },
+    totalTime: (id) => {
+        let total = Timesheet.find({
+            owner: Meteor.userId(),
+            active: false
+        }).fetch()
+
+        let active = Timesheet.findOne({
+            owner: Meteor.userId(),
+            active: true
+        })
+
+        let user = Meteor.users.findOne({
+            _id: Meteor.userId()
+        }) || {}
+
+        let duration = total.reduce((i1, i2) => i1 + i2.totalTime, 0)
+
+        if (active) {
+            duration += (Template.instance().timer.get() - active.startTime) + (active.totalTime || 0)
+        }
+
+        let dec = duration / (1000 * 60 * 60)
+
+        return {
+            formattedTime: formatDuration(moment.duration(duration)),
+            decimalTime: dec,
+            earnings: dec * ((user.profile) || {}).hourlyRate || 0
+        }
     },
     total: () => {
         let total = Timesheet.find({
@@ -67,7 +116,7 @@ Template.paymentsview.helpers({
             earnings: dec * ((user.profile) || {}).hourlyRate || 0
         }
     },
-    totalTime: function () {
+    totalTime: function() {
         let duration
 
         if (this.active) {
@@ -86,18 +135,40 @@ Template.paymentsview.helpers({
 })
 
 Template.paymentsview.events({
-    'click .paid': function(event, templateInstance) {
-        event.preventDefault()
+    'change .earningsCheckbox': function(event, templateInstance) {
 
-        markAsPaid.call({
-            paymentId: FlowRouter.getParam("paymentId")
-        }, (err, data) => {
-            if (err) {
-                notify(err.reason || err.message, 'error')
-            } else {
-                FlowRouter.go('/')
-                notify('Marked as Paid', 'success')
-            }
-        })
+        let updateEarnings = calculateEarnings();
+        templateInstance.approvedEarnings.set(updateEarnings);
+
+    },
+    'click .paid': function(event, templateInstance) {
+        event.preventDefault();
+        let approvedTimesheets = [];
+        let notApprovedTimesheets = [];
+        let approvedEarnings = Template.instance().approvedEarnings.get();
+
+        $("input[type=checkbox]:checked").each(function() {
+            approvedTimesheets.push(this.id)
+        });
+
+        $("input:checkbox:not(:checked)").each(function() {
+            notApprovedTimesheets.push(this.id)
+        });
+
+            markAsPaid.call({
+                paymentId : FlowRouter.getParam("paymentId"),
+                approvedTimesheets: approvedTimesheets,
+                notApprovedTimesheets: notApprovedTimesheets
+            }, (err, data) => {
+                if (err) {
+                    notify(err.reason || err.message, 'error')
+                } else {
+                    FlowRouter.go('/')
+                    notify('Marked as Paid', 'success')
+                }
+            })
+
+
+
     }
 })
