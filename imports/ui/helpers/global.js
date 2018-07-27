8e815f3a2e74fc479b54d8647454674301bf8014
Dilpreet Singh <singhsince94@gmail.com>
2018-07-27 21:05:11 +0530

    problem: users can start work before being approved #63
    solution: adds a 'pending' warning bar at the top of the page until the user has been approved by a moderator.

--- imports/ui/helpers/global.js
+++ imports/ui/helpers/global.js
@@ -1,3 +1,5 @@
-import { isModerator } from '/imports/api/users/methods'
+import { isModerator, approvedUser } from '/imports/api/users/methods'
 
 Template.registerHelper('isModerator', () => isModerator(Meteor.userId()))
+
+Template.registerHelper('unapprovedUser', () => !approvedUser(Meteor.userId()))
